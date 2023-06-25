9805dd3d878856764a8d1d326c8719e505803179
JunYan101 <61654255+chenjunyan1@users.noreply.github.com>
2023-06-25 10:04:51 +0800

    update

--- index.js
+++ index.js
@@ -9,20 +9,22 @@
 // fib(4) => 5
 // fib(6) => 13
 
-// function fib(index) {
-//   // index-1 + index-2
-//   // 截止
-//   if (index <= 1) {
-//     return 1;
-//   }
+// 通过递归实现
+function fib(index) {
+  // index-1 + index-2
+  // 截止
+  if (index <= 1) {
+    return 1;
+  }
 
-//   return fib(index - 1) + fib(index - 2);
-// }
+  return fib(index - 1) + fib(index - 2);
+}
 
+// ----------------------------------------------------
 // value: 1, 1, 2, 3, 5, 8, ...
 // index: 0, 1, 2, 3, 4, 5, ...
 
-function fib(index) {
+function fib_2(index) {
   // for index count
   // 前两个数字之和
   let num_1 = 1;
@@ -42,6 +44,54 @@ function fib(index) {
   }
 }
 
-console.log(fib(6));
+// console.log(fib(6));
 
-// livia@nissispace.com
+//----------------------------------------------------
+// value: 1, 1, 2, 3, 5, 8, ...
+// index: 0, 1, 2, 3, 4, 5, ...
+
+// js中的闭包来实现,优点是可以提高取值速速且不占用全局变量
+const fib_3 = (() => {
+  //  index 取值范围 1-10000
+  let fibArr = [1, 1];
+
+  for (let index_1 = 0; index_1 < 10000; index_1++) {
+    let newFibNum = fibArr[index_1] + fibArr[index_1 + 1];
+    fibArr.push(newFibNum);
+  }
+
+  return (index) => {
+    return fibArr[index];
+  };
+})();
+
+// console.log(fib_3(5));
+
+//----------------------------------------------------
+// 使用闭包，通过 hashMap 的缓存来实现，优点相较于上面的那种是可以不限制 index范围
+// value: 1, 1, 2, 3, 5, 8, ...
+// index: 0, 1, 2, 3, 4, 5, ...
+
+const fib_4 = (() => {
+  const hashMap = new Map();
+
+  return (index) => {
+    // 如果计算过该 index 就直接从 hashMap 中返回该 index 对应的 value
+    if (hashMap.has(index)) {
+      console.log("已经计算过了");
+      return hashMap.get(index);
+    } else {
+      let num_1 = 1;
+      let num_2 = 1;
+      for (let index_2 = 0; index_2 < index - 1; index_2++) {
+        let cloneNum_2 = num_2;
+        num_2 = num_1 + num_2;
+        num_1 = cloneNum_2;
+      }
+      hashMap.set(index, num_2);
+      return num_2;
+    }
+  };
+})();
+
+console.log(fib_4(5));
